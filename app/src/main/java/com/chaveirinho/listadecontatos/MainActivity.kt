677579825594c50998ba9ed0d4385c86665bff6d
maria-alane <maria.alane.bs@gmail.com>
2022-02-01 18:26:25 -0300

    Options menus

--- app/src/main/java/com/chaveirinho/listadecontatos/MainActivity.kt
+++ app/src/main/java/com/chaveirinho/listadecontatos/MainActivity.kt
@@ -2,6 +2,10 @@ package com.chaveirinho.listadecontatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -32,8 +36,42 @@ class MainActivity : AppCompatActivity() {
                     "Mari Alane",
                     "(00) 0000-0000",
                     "img.png"
+                ),
+                Contact(
+                    "Eduardo",
+                    "(00) 0000-0000",
+                    "img.png"
+                ),
+                Contact(
+                    "Nina Mudock",
+                    "(00) 0000-0000",
+                    "img.png"
                 )
             )
         )
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_menu_1 -> {
+                showToast("Exibindo item menu 1")
+                true
+            }
+            R.id.item_menu_2 -> {
+                showToast("Exibindo item menu 2")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
